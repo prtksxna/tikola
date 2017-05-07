8228dfb29043eadc45767bcf91f69c3607eeda81
Prateek Saxena <prtksxna@gmail.com>
2017-05-07 12:07:36 +0530

    feat: show images in thumbnails

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,39 +9,21 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_single() ) :
+<?php
+if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php tikola_posted_on(); ?>
-			<?php the_post_thumbnail(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
+else: ?>
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'tikola' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'tikola' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php tikola_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+<article id="post-<?php the_ID(); ?>" <?php post_class('col-md-3'); ?>>
+	<div class="entry-meta">
+		<a
+			class="thumbnail-link"
+			title="<?php the_title(); ?>"
+			style="background-image:url(<?php the_post_thumbnail_url(); ?>)"
+			href="<?php the_permalink();  ?>"
+		></a>
+	</div><!-- .entry-meta -->
 </article><!-- #post-## -->
+
+<?php
+endif; ?>
