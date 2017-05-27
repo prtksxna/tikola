2e1750512d5dd105ff62fc7335df3f54db42b61a
Prateek Saxena <prtksxna@gmail.com>
2017-05-27 23:12:20 +0530

    Fix header

--- header.php
+++ header.php
@@ -38,13 +38,6 @@
 						} ?>
 					</a>
 				</h1>
-
-				<?php
-				$description = get_bloginfo( 'description', 'display' );
-				if ( $description || is_customize_preview() ) : ?>
-					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-				<?php
-				endif; ?>
 			</div><!-- .site-branding -->
 
 			<nav id="site-navigation" class="main-navigation col-md-6" role="navigation">
