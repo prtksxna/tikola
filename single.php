16ba219f807d891bef78887858deb1f113b71111
Prateek Saxena <prtksxna@gmail.com>
2017-05-27 15:36:33 +0530

    Add collection template-part and archive page

--- single.php
+++ single.php
@@ -14,8 +14,11 @@ get_header(); ?>
 
 		<?php
 		while ( have_posts() ) : the_post();
-
-			get_template_part( 'template-parts/content', get_post_format() );
+			if ( in_category( 'collection' ) ) {
+				get_template_part( 'template-parts/content-collection', get_post_format() );
+			} else {
+				get_template_part( 'template-parts/content', get_post_format() );
+			}
 
 			the_post_navigation();
 
