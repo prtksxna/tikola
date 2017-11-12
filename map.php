c30aa240f490770a2a6c0abf19693a5a21eced99
Prateek Saxena <prtksxna@gmail.com>
2017-11-12 08:01:45 +0530

    Add map to map page

--- map.php
+++ map.php
@@ -10,16 +10,35 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css"
+  integrity="sha512-M2wvCLH6DSRazYeZRIm1JnYyh22purTM+FDB5CsyxtQJYeKq83arPe5wgbNmcFXGqiSH2XR8dT/fJISVA1r/zQ=="
+  crossorigin=""/>
+<script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"
+  integrity="sha512-lInM/apFSqyy1o6s89K4iQUKg6ppXEgsVxT35HbzUupEVRh2Eu9Wdl4tHj7dZO0s1uvplcYGmt3498TtHq+log=="
+  crossorigin=""></script>
+
+<script src="https://unpkg.com/leaflet.markercluster@1.2.0/dist/leaflet.markercluster.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.2.0/dist/MarkerCluster.css"/>
+<link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.2.0/dist/MarkerCluster.Default.css"/>
+
+<script>
+window.locations =
 		<?php
 			$the_query = new WP_Query( array( 'cat' => 2 ) );
 // The Loop
 if ( $the_query->have_posts() ) {
+	echo "[";
 	while ( $the_query->have_posts() ) {
+		echo "{";
 		$the_query->the_post();
-		the_title();
-		echo get_post_meta( get_the_id(), 'geo_latitude' )[0];
-		echo get_post_meta( get_the_id(), 'geo_longitude' )[0];
+		echo "\"title\":\"" . get_the_title() . "\",";
+		echo "\"latitude\":\"" . get_post_meta( get_the_id(), 'geo_latitude' )[0] . "\",";
+		echo "\"longitude\":\"" . get_post_meta( get_the_id(), 'geo_longitude' )[0] . "\",";
+		echo "\"url\":\"" . get_the_permalink() . "\",";
+		echo "\"photo\":\"" . get_the_post_thumbnail_url()  . "\"";
+		echo "},";
 	}
+	echo "];";
 	/* Restore original Post Data */
 	wp_reset_postdata();
 } else {
@@ -27,7 +46,13 @@ if ( $the_query->have_posts() ) {
 }
 
 		?>
+</script>
 
+<div id="map"></div>
+<?php
+# TODO: There has to be a better way
+wp_enqueue_script( 'map', '/wp-content/themes/tikola/js/map.js', ['jquery'], 0, true);
+?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
