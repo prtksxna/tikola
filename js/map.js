a6f90c2e5c3cf65fda95f7337224869b91deb6fd
Prateek Saxena <prtksxna@gmail.com>
2017-11-12 12:12:00 +0530

    Use mapbox light #16

--- js/map.js
+++ js/map.js
@@ -4,7 +4,7 @@ $( function () {
 	var map = L.map( 'map' ).setView([51.505, -0.09], 13);
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 		maxZoom: 18,
-		id: 'mapbox.dark',
+		id: 'mapbox.light',
 		accessToken: 'sk.eyJ1IjoicHJ0a3N4bmEiLCJhIjoiY2o5dGxkNHNjMzRhcDMybWlnd3Fkb3BuNiJ9.7z36O9zk3WJ-LzO0Rdh-vw'
 	}).addTo(map);
 
