631991a77238512e1a89cab3244d7afcbb224e48
Prateek Saxena <prtksxna@gmail.com>
2017-11-12 08:17:59 +0530

    Add popup to map marker

--- js/map.js
+++ js/map.js
@@ -1,4 +1,6 @@
-jQuery( function () {
+var $ = jQuery;
+
+$( function () {
 	var map = L.map( 'map' ).setView([51.505, -0.09], 13);
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 		maxZoom: 18,
@@ -9,7 +11,21 @@ jQuery( function () {
 	var markers = L.markerClusterGroup();
 	window.locations.forEach( function ( location ) {
 		if ( location.latitude !== '' && location.longitude !== '' ) {
-			markers.addLayer(L.marker([location.latitude, location.longitude]));
+			var marker = L.marker([location.latitude, location.longitude]);
+			var $popup = $( '<span>' );
+
+			$popup.append( $( '<a>' )
+				.attr( 'href', location.url )
+				.text( location.title )
+				.prepend( $( '<img>' )
+					.attr( 'src', location.photo )
+					.attr( 'alt', location.title )
+					.attr( 'width', 100 )
+				)
+			);
+
+			marker.bindPopup( $popup[0].outerHTML );
+			markers.addLayer( marker );
 		}
 	} );
 	map.addLayer(markers);
